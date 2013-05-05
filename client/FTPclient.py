8fd287877d93d69bcc5e5b01675f5e1743e86b4d
Amod Mulay <amodmulay1@gmail.com>
2013-05-05 01:14:56 -0400

    Everything except data port working

--- client/FTPclient.py
+++ client/FTPclient.py
@@ -1,1 +1,105 @@
- 
+#!/usr/bin/python3
+from socket import *
+import os, sys
+#from time import clock
+if(len(sys.argv) > 1):
+    serverName = sys.argv[1]
+else:
+    serverName = 'localhost'
+if(len(sys.argv) > 2):
+    serverPortBase = int(sys.argv[2])
+else:
+    serverPortBase = 13000
+command = "getSocket"
+clientSocket = socket(AF_INET,SOCK_STREAM)
+clientSocket.connect((serverName, serverPortBase))
+clientSocket.send(bytearray(command.encode("utf-8")))
+#startTime = clock()
+response = clientSocket.recv(1024)
+response = response.decode("utf-8")
+print("Session socket:"+response)
+serverPort = int(response)
+clientSocket.close()
+while 1: 
+    command = input('Enter Command:')   
+    if command.lower() == "bye" or command.lower() == "quit":
+        clientSocket = socket(AF_INET,SOCK_STREAM)
+        clientSocket.connect((serverName, serverPort))
+        command = "releaseSocket " + str(serverPort)
+        clientSocket.send(bytearray(command.encode("utf-8")))
+        break
+    elif command.lower() == "server exit":
+        clientSocket = socket(AF_INET,SOCK_STREAM)
+        clientSocket.connect((serverName, serverPortBase))
+        #command = "releaseSocket " + str(serverPort)
+        clientSocket.send(bytearray(command.encode("utf-8")))
+        break
+    elif (command.split()[0]).lower() == "get":
+        clientSocket = socket(AF_INET,SOCK_STREAM)
+        clientSocket.connect((serverName, serverPort))
+        myfileName = command.split()[1]
+        clientSocket.send(bytearray("list".encode("utf-8")))
+        fileList = clientSocket.recv(1024).decode("utf-8")
+        #print(fileList)
+        #print(fileList.split('\n  '))
+        #print(" "+myfileName )
+        #print(myfileName in fileList.split('\n  '))
+        fileExists = "False"
+        for x in fileList.split('\n  '):
+            if x == myfileName:
+                fileExists = "True"
+        if fileExists == "True":
+            clientSocket = socket(AF_INET,SOCK_STREAM)
+            clientSocket.connect((serverName, serverPort))
+            clientSocket.send(bytearray(command.encode("utf-8")))
+            response = clientSocket.recv(1024)
+            fileSize = int(response.split()[0].decode("utf-8"))
+            #print(fileSize)
+            #fileSize = int(fileSize.decode("utf-8"))
+            #response = clientSocket.recv(fileSize)
+            print(" Filesize at sender: " + str(fileSize) + " bytes")
+            if (len(response.split()) > 1):
+                    dataPosition = response.find(" StartFile".encode("utf-8"))
+                    myfileContent = response[(dataPosition + len(" StartFile".encode("utf-8")) ):]
+            else:
+                myfileContent = clientSocket.recv(1024)
+            while len(myfileContent) < fileSize:
+                myfileContent += clientSocket.recv(1024)
+                print(" "+str(int(len(myfileContent)*100.0/fileSize)) + "% Transfer Complete", end="\r")
+            print(" File size recieved: " + str(len(myfileContent)) + " bytes")
+            #myfileContent = response
+            myfile = open(myfileName, 'wb')
+            myfile.write(myfileContent)
+            myfile.close()
+            print("  File: "+myfileName+" received")
+        else:
+            print(" File "+myfileName+" not found at server")
+    elif (command.split()[0]).lower() == "put":
+        clientSocket = socket(AF_INET,SOCK_STREAM)
+        clientSocket.connect((serverName, serverPort))
+        myfileName = " ".join(command.split()[1:])#deletes the preceding command name
+        #print(myfileName)
+        if os.path.isfile(myfileName) == False:
+            print(" File "+myfileName+" not found")
+        else:
+            myfile = open(myfileName, "rb")
+            myfileContent = myfile.read()
+            fileSize = len(myfileContent)
+            clientSocket.send(bytearray((command+" "+str(fileSize)+" StartFile").encode("utf-8")))
+            #clientSocket.send(bytearray((" "+str(fileSize)+" ").encode("utf-8")))
+            clientSocket.send(bytearray(myfileContent))
+            myfile.close()
+            clientSocket.close()
+            print("  File: "+myfileName+" sent")
+    else:
+        clientSocket = socket(AF_INET,SOCK_STREAM)
+        clientSocket.connect((serverName, serverPort))
+        clientSocket.send(bytearray(command.encode("utf-8")))
+        #startTime = clock()
+        response = clientSocket.recv(1024)
+        response = response.decode("utf-8")
+        print("Server response:"+response)
+        clientSocket.close()
+        if command == "server exit":
+            break
+print("Exited Client") 
--- server/FTPserver.py
+++ server/FTPserver.py
@@ -1,1 +1,166 @@
- 
+#!/usr/bin/python3
+from socket import *
+import os, sys
+import datetime
+import threading
+
+if(len(sys.argv) > 1):
+    serverPort = int(sys.argv[1])
+else:
+    serverPort = 13000
+serverDataPortStart = serverPort #set to zeroth port of node-servers
+serverSocketBase = socket(AF_INET,SOCK_STREAM)
+serverSocketBase.bind(('',serverPort))
+serverSocketBase.listen(1)
+maxClients = 5
+clientSocketState = maxClients * [0]
+#print(clientSocketState)
+serverSocket = maxClients * [0]
+for x in range(maxClients):
+    serverSocket[x] = socket(AF_INET,SOCK_STREAM)
+    serverSocket[x].bind(('',(serverDataPortStart+x+1)))
+    serverSocket[x].listen(1) 
+print("The server is ready to recieve")
+
+
+def resolve_command(command, connectionSocket):
+    connectionSocket = connectionSocket
+    returnVal = 0
+    if 1==1: #place holder in case while has to be implemented at some later time
+        if command == "help":
+            #enter command options and help menu here
+            response = """This is a list of available server commands
+            1. help : brings up this menu
+            2. server exit : exits both server and client processes
+            3. get <filename> : gets <filename> from server
+            4. put <filename> : puts <filename> to server
+            5. list : lists the contents of working directory at server
+            6. bye / quit : Either of the commands can be used to exit client
+            """
+            connectionSocket.send(bytearray(response.encode("utf-8")))
+            connectionSocket.close()
+#getSocket
+        elif command == "getSocket":
+            response = str(clientSocketState.index(0) + 1 + serverDataPortStart)
+            clientSocketState[clientSocketState.index(0)] = 1
+            connectionSocket.send(bytearray(response.encode("utf-8")))
+            connectionSocket.close()
+#releaseSocket
+        elif command.split()[0] == "releaseSocket":
+            response = " ".join(command.split()[1:]) #deletes the preceding command name
+            tempSocket = int(response)
+            clientSocketState[tempSocket - 1 - serverDataPortStart] = 0  
+            #response = "socket clear"
+            #print(response)
+            #connectionSocket.send(bytearray(response.encode("utf-8")))
+            connectionSocket.close()
+#server exit
+        elif command == "server exit":
+            response = "TCPServer exiting"
+            connectionSocket.send(bytearray(response.encode("utf-8")))
+            connectionSocket.close()
+            returnVal = 1
+#list
+        elif (command.split()[0]).lower() == "list":
+            if(len(command.split()) > 1):
+                response = os.listdir(command.split()[1].lower())
+            else:
+                response = os.listdir('.')
+            temp = ''
+            #print(response)
+            #for x in response:
+            #    temp =  '\n  ' + temp + str(x) 
+            temp = "\n  " + "\n  ".join(response)
+            #print(temp)
+            connectionSocket.send(bytearray(temp.encode("utf-8")))
+            connectionSocket.close()
+#cd
+        elif (command.split()[0]).lower() == "cd":
+            os.chdir(command.split()[1])
+            response = "Working directory changed to: " + str(os.getcwd())
+            connectionSocket.send(bytearray(response.encode("utf-8")))
+            connectionSocket.close()
+#get
+        elif (command.split()[0]).lower() == "get":
+            myfileName = command.split()[1] #deletes the preceding command name
+            myfile = open(myfileName, "rb")
+            response = myfile.read()
+            fileSize = len(response)
+            connectionSocket.send(bytearray((str(fileSize)+" StartFile").encode("utf-8")))
+            connectionSocket.send(bytearray(response))
+            myfile.close()
+            connectionSocket.close()
+            print("  File: "+myfileName+" sent")
+#put
+        elif type(command) != type("string"):
+            if(command.split()[0].decode("utf-8")).lower() == "put":
+                myfileName = command.split()[1].decode("utf-8")
+                fileSize = command.split()[2].decode("utf-8")
+                print(" Filesize at sender: " + str(fileSize) + " bytes")
+                #print(fileSize)
+                fileSize = int(fileSize)#.decode("utf-8"))
+                #response = connectionSocket.recv(fileSize)
+                if (len(command.split()) > 4):
+                    dataPosition = command.find(" StartFile".encode("utf-8"))
+                    myfileContent = command[(dataPosition + len(" StartFile".encode("utf-8")) ):]
+                    #print(myfileContent)
+                #myfileContent += response
+                #else:
+                #    myfileContent = response
+                #    print("File size recieved: " + str(len(myfileContent)))
+                myfileContent = connectionSocket.recv(1024)
+                while len(myfileContent) < fileSize:
+                    myfileContent += connectionSocket.recv(1024)
+                    print(" "+str(int(len(myfileContent)*100.0/fileSize)) + "% Transfer Complete", end="\r")
+                print(" File size recieved: " + str(len(myfileContent)) + " bytes")
+                myfile = open(myfileName, 'wb')
+                myfile.write(myfileContent)
+                myfile.close()
+                connectionSocket.close()
+                print("  File: "+myfileName+" received")
+        else:
+            response = "Unknown Command"
+            connectionSocket.send(bytearray(response.encode("utf-8")))
+            connectionSocket.close()
+    return returnVal 
+
+class serverThread (threading.Thread):
+    def __init__(self, threadID):
+        self.threadID = threadID
+        threading.Thread.__init__(self)
+    def run(self):
+        while 1:
+            #workingDir = os.getcwd()
+            connectionSocket, addr = serverSocket[self.threadID].accept()
+            command = connectionSocket.recv(1024)
+            print('Thread Recieved: ', command)
+            #print(len(command.split()))
+            if(command.split()[0].decode("utf-8")).lower() != "put": #skips decode if command is put
+                command = command.decode("utf-8")
+            #print(command)
+            returnVal = resolve_command(command,connectionSocket)
+            if(returnVal==1):
+                break
+
+
+#initializing threads for clients
+for x in range(maxClients):
+    temp = serverThread(x)
+    temp.start()
+
+
+
+while 1:
+    connectionSocket, addr = serverSocketBase.accept()
+    command = connectionSocket.recv(1024)
+    print('Main Recieved: ', command)
+    command = command.decode("utf-8")
+    #print(command)
+    returnVal = resolve_command(command,connectionSocket)
+    if(returnVal==1):
+        break
+for x in range(maxClients):
+    serverSocket[x].close()
+serverSocketBase.close()
+
+
